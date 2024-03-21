f93c3c7b9a6ec3112311a428d8a7cf17c33988be
Sami Ullah <sami.ullah@invozone.com>
2024-03-21 13:44:10 +0500

    add env variables types

--- app.ts
+++ app.ts
@@ -1,2 +1,14 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 export const app = express();
+
+//body parse
+app.use(express.json({ limit:  "50mb" }))
+
+app.use(cookieParser())
+
+app.use(cors({
+    origin: process.env.ORIGIN,
+}))
---
+++ type.ts
@@ -0,0 +1,23 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        PORT: string;
+        ORIGIN: string;
+    }
+}
+
+/*
+Zode expample for env
+Zod will ensure the environment variables you defined match the schema and it will remove 
+from the final object any environment variable not defined in the schema. 
+This way your app can't read any other environment variable you didn't explicitly defined.
+
+import { z } from "zod";
+
+let envSchema = z.object({
+  GITHUB_CLIENT_ID: z.string().nonempty(),
+  GITHUB_CLIENT_SECRET: z.string().nonempty(),
+  STRIPE_API_KEY: z.string().nonempty(),
+});
+let env = envSchema.parse(process.env);
+
+*/
